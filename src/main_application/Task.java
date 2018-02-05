415c2f53397c865146b8d6c4b8e5366473567947
Michael Skrzypietz <michael.s97@web.de>
2018-02-05 15:29:58 +0100

    improved the index calculation for the previous and next primes in the arraylist

--- src/main_application/Task.java
+++ src/main_application/Task.java
@@ -1,17 +1,10 @@
 package main_application;
 
-import algorithms.PollardRho;
+import algorithms.Factorizer;
 import algorithms.PrimeListGenerator;
-import base.Partition;
 
-import java.math.BigInteger;
 import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
 import java.util.concurrent.CyclicBarrier;
-import java.util.stream.Collectors;
-
-import static java.lang.System.currentTimeMillis;
 
 public class Task implements Runnable {
 
@@ -19,8 +12,7 @@ public class Task implements Runnable {
     private Integer minimum;
     private Integer maximum;
     private ArrayList<Integer> primes;
-    private Integer currentPreviousPrimeIndex;
-    private Integer currentNextPrimeIndex;
+    private Integer currentPreviousPrimeIndex = 0;
 
     public Task(CyclicBarrier cyclicBarrier, Integer minimum, Integer maximum) {
         this.cyclicBarrier = cyclicBarrier;
@@ -34,19 +26,6 @@ public class Task implements Runnable {
         primes.add(0, getPreviousPrime(minimum));
         primes.add(getNextPrime(maximum));
         System.out.println(this.toString() + ": Primes initiated");
-    }
-
-    private void initCurrentPreviousPrimeIndex() {
-        this.currentPreviousPrimeIndex = 0;
-    }
-
-    private void initCurrentNextPrimeIndex() {
-        for (int i = 1;; i++) {
-            if (primes.get(i) > minimum) {
-                this.currentNextPrimeIndex = i;
-                break;
-            }
-        }
     }
 
     private Integer getPreviousPrime(int minimum) {
@@ -74,40 +53,26 @@ public class Task implements Runnable {
 
     private void execute() {
         initPrimesList();
-        initCurrentPreviousPrimeIndex();
-        initCurrentNextPrimeIndex();
-        PollardRho pollardRho = new PollardRho();
+        Factorizer factorizer = new Factorizer();
 
         for (int i = minimum; i <= maximum; i++) {
-            // 1. get all prime factors
-            // 2. get sum of all prime factors
-            // 3. find previous prime of i
-            // check if i - (2.) == (3.)
-            //      -> false: continue to next number
-            //      -> true: 4. find next prime of i
-            //              check if i + (2.) == (4.)
-            //                  -> false: continue to next number
-            //                  -> true: falsified the assumption!
-
-            //if (i % 10 == 0)
-            //    System.out.println(i);
-            List<BigInteger> factors = pollardRho.factor(BigInteger.valueOf(i));
-            BigInteger sumOfFactors = factors.stream().reduce(BigInteger.ZERO, BigInteger::add);
+            Integer sumOfFactors = factorizer.sumOfPrimeFactors(i);
             while (i > primes.get(currentPreviousPrimeIndex + 1)) {
                 currentPreviousPrimeIndex++;
             }
             int previousPrime = primes.get(currentPreviousPrimeIndex);
-            if (i - sumOfFactors.intValue() != previousPrime)
+            if (i - sumOfFactors != previousPrime)
                 continue;
+            // index of the next prime is currentPreviousPrimeIndex + 1 unless the current number is a prime then
+            // the next prime is currentPreviousPrimeIndex + 2
             int nextPrime = i == primes.get(currentPreviousPrimeIndex + 1) ? primes.get(currentPreviousPrimeIndex + 2) : primes.get(currentPreviousPrimeIndex + 1);
             System.out.println("Number = " + i + "; sumOfFactors = " + sumOfFactors + "; previous prime = " + previousPrime + "; next prime = " + nextPrime);
-            if (i + sumOfFactors.intValue() != nextPrime)
+            if (i + sumOfFactors != nextPrime)
                 continue;
             System.out.println("ASSUMPTION FALSIFIED!!!! " + i + " is smaller with the same properties");
-            System.out.println("Sum of the prime factors of i =" + sumOfFactors.intValue());
+            System.out.println("Sum of the prime factors of i =" + sumOfFactors);
             break;
         }
-
     }
 
     public void run() {
